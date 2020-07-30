e8c2d476384009930348282b550ee32101fdb37c
RIYA2410 <riyajainangel675@gmail.com>
2020-07-30 11:04:58 -0700

    committed

---
+++ sms.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = 'ACda5070f7c1c09affa939ed7f3547e036'
+auth_token = '68abbebbd06d52c3d45b15cc75cd969b'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+	                            from_= '+14437753527',
+	                            body= 'helllooooo',
+	                            to= '+916388972557'
+	                            )
+
+print(message.sid)
